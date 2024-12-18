4ea11b6e9b00b8e64cd78c73d674f4f450303cbb
RenaTheDv <thenefill@gmail.com>
2024-12-18 15:30:44 +0000

    Исправление ошибки в коде, деплой в облако

--- pages/plot.py
+++ pages/plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import random
-зш
+
 #Код sidebar'a
 st.sidebar.title('Доступные страницы:')
 st.sidebar.write('__MAIN__ - основная страница с информацией про компанию Apple...')
